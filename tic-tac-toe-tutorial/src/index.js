5c2bb8e313599abe27664f663588bdcea8b33b2a
Rahul Rakshit <rahulrakshit@gmx.com>
2018-07-06 12:26:46 +0100

    play with react dev tools from tutorial

--- tic-tac-toe-tutorial/src/index.js
+++ tic-tac-toe-tutorial/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 // I've inspected the starter code
+// I've installed the react dev tools and played with them
 
 class Square extends React.Component {
   constructor(props) {
