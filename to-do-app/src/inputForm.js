a64d41c72ba5df8fe31954d52524038b81790115
Rahul Rakshit <rahulrakshit@gmx.com>
2018-07-09 17:19:11 +0100

    build alert-form without HTML-form

--- to-do-app/src/inputForm.js
+++ to-do-app/src/inputForm.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 class InputForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+    this.changeHandler = (event) => {
+      this.setState({text: event.target.value});
+    }
+    this.clickHandler = (event) => {
+      event.preventDefault();
+      alert(this.state.text);
+      this.setState({text: ''});
+    }
+  }
   render() {
     return (
       <div>
-        <input type="text" />
-        <input type="submit" />
+        <input type="text" value={this.state.text} onChange={this.changeHandler}/>
+        <button onClick={this.clickHandler}>Submit</button>
       </div>)
   }
 }
