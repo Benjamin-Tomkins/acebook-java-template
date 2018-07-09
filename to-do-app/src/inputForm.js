f7402ddfa317be9e5813d556951b84974c8fa28a
Rahul Rakshit <rahulrakshit@gmx.com>
2018-07-09 18:27:09 +0100

    lift state of input field up

--- to-do-app/src/inputForm.js
+++ to-do-app/src/inputForm.js
@@ -3,21 +3,14 @@ import React from 'react';
 class InputForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {text: ''};
-    this.changeHandler = (event) => {
-      this.setState({text: event.target.value});
-    }
-    this.clickHandler = (event) => {
-      event.preventDefault();
-      alert(this.state.text);
-      this.setState({text: ''});
-    }
+    this.handleChange = this.props.changeHandler;
+    this.handleClick = this.props.clickHandler;
   }
   render() {
     return (
       <div>
-        <input type="text" value={this.state.text} onChange={this.changeHandler}/>
-        <button onClick={this.clickHandler}>Submit</button>
+        <input type="text" value={this.props.value} onChange={this.handleChange}/>
+        <button onClick={this.handleClick}>Create Note</button>
       </div>)
   }
 }
--- to-do-app/src/toDoApp.js
+++ to-do-app/src/toDoApp.js
@@ -6,22 +6,33 @@ class ToDoApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      notes: ["note 1", "note 2", "note 3"]
+      notes: ["note 1", "note 2", "note 3"],
+      newNote: 22
     }
-    this.handleClick = () => alert("Fuck off!");
+    this.noteHandleClick = () => alert("Fuck off!");
+    this.newNoteHandleChange = (event) => {
+      event.preventDefault();
+      this.setState({newNote: event.target.value});
+      this.forceUpdate();
+    }
+    this.handleCreateNote = () => {
+      alert(this.state.newNote);
+      this.setState({newNote: ''});
+      this.forceUpdate();
+    }
     this.renderNote = (note, index) => {
-      return <ToDoNote key={index} value={note} onClick={this.handleClick} />
+      return <ToDoNote key={index} value={note} onClick={this.noteHandleClick} />
     }
     this.renderNotes = () => {
       return this.state.notes
         .map((note, index) => this.renderNote(note, index));
-      }
+    }
   }
 
   render() {
     return (
       <div>
-        <InputForm />
+        <InputForm value={this.state.newNote} changeHandler={this.newNoteHandleChange} clickHandler={this.handleCreateNote} />
         <ul>{this.renderNotes()}</ul>
       </div>
     )
