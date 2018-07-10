1edf9e729a62844bc669118a4984d5f68043b5b2
Rahul Rakshit <rahulrakshit@gmx.com>
2018-07-10 14:56:55 +0100

    add catch for errors

--- playing-with-apis/src/index.js
+++ playing-with-apis/src/index.js
@@ -8,10 +8,13 @@ class App extends React.Component{
     this.state = {text: "nothing has been fetched yet" }
   }
   componentDidMount() {
-    $.get("https://chitter-backend-api.herokuapp.com/users", (response) => {
-      let kay = response[0].handle
-      this.setState({text: kay})
-    })
+
+    $.get("https://chitter-backend-api.herokuapp.com/usfdsaers")
+      .then((response) => {
+        let kay = response[0].handle
+        this.setState({text: kay})
+      })
+      .catch((jqXHR) => console.log(`Hello, my favorite number is ${jqXHR.status}`));
   }
   render() {
     return <h1> {this.state.text} </h1>
