f4d6035deb8a2b65601b03f2307e54202a16961f
Rahul Rakshit <rahulrakshit@gmx.com>
2018-07-11 10:57:46 +0100

    make initial get request with axios go on screen

--- frontend-v1/src/index.js
+++ frontend-v1/src/index.js
@@ -2,22 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'axios';
 
-class HelloKay extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {response: 'nothing has been fetched yet'};
+    this.domain = 'https://chitter-backend-api.herokuapp.com';
+    this.port = '443';
+    this.state = {posts: 'nothing has been fetched yet'};
   }
 
   componentDidMount() {
-    $.get('https://chitter-backend-api.herokuapp.com/users')
-      .then((response) => {
-        this.setState({response: JSON.stringify(response)});
+    $.get(`${this.domain}:${this.port}/users`)
+      .then(rawResponse => rawResponse.data)
+      .then(people => {
+        return people.map(person => person.handle);
+      })
+      .then(peopleNames => {
+        this.setState({posts: JSON.stringify(peopleNames)});
       });
   }
 
   render() {
-    return <h1>{this.state.response}</h1>
+    return <h1>{this.state.posts}</h1>
   }
 }
 
-ReactDOM.render(<HelloKay />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
