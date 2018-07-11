eefd28cbbd7b01c77d56692dce0f9f1da8fcf056
Rahul Rakshit <rahulrakshit@gmx.com>
2018-07-11 16:06:46 +0100

    get request works; post request raises 403

--- frontend-v1/src/app.js
+++ frontend-v1/src/app.js
@@ -3,30 +3,35 @@ import $ from 'axios';
 import InputForm from './inputForm';
 import Posts from './posts';
 
+// $.defaults.headers.post['Content-Type'] = 'application/json';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.domain = 'https://chitter-backend-api.herokuapp.com';
-    this.port = '443';
+    this.domain = 'http://192.168.48.112';
+    this.port = '3000';
+    this.user_id = '1001';
     this.state = {posts: []};
     this.refreshPosts = () => {
-      // $.get(`${this.domain}`:`${this.port}/users/99999999/posts/`)
-      //   .then(rawResponse => rawResponse.data)
-      //   .then(posts => posts.map(post => post.post_text))
-      //   .then(postStrings => {
-      //     this.setState({posts: postStrings})
-      //   });
-      $.get(`${this.domain}:${this.port}/users`)
+      $.get(`${this.domain}:${this.port}/users/${this.user_id}/posts/`)
         .then(rawResponse => rawResponse.data)
-        .then(people => {
-          return people.map(person => person.handle);
-        })
-        .then(peopleNames => {
-          this.setState({posts: peopleNames});
+        .then(posts => posts.map(post => post.post_text))
+        .then(postStrings => {
+          this.setState({posts: postStrings})
         });
+      // $.get(`${this.domain}:${this.port}/users`)
+      //   .then(rawResponse => rawResponse.data)
+      //   .then(people => {
+      //     return people.map(person => person.handle);
+      //   })
+      //   .then(peopleNames => {
+      //     this.setState({posts: peopleNames});
+      //   });
     };
     this.makePost = (postText) => {
-      // $.post(`${this.domain}`:`${this.port}/users/99999999/posts/`, JSON.stringify(postText));
+      console.log(postText);
+      console.log(this.user_id);
+      $.post(`${this.domain}:${this.port}/users/${this.user_id}/posts/`, {"post_text": postText}, {headers: {'Content-Type': 'applications/json'}});
       alert(`Your post "${postText}" has been made.`);
       this.forceUpdate();
     }
