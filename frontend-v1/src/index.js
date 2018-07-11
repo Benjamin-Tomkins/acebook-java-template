fe9203f75fa2449c23160a819da9177c69c33170
Rahul Rakshit <rahulrakshit@gmx.com>
2018-07-11 11:10:04 +0100

    show names using chitter challenge API

--- frontend-v1/src/index.js
+++ frontend-v1/src/index.js
@@ -2,12 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'axios';
 
+class Posts extends React.Component {
+  constructor(props) {
+    super(props)
+    this.renderPosts = () => {
+      return this.props.names.map((name, index) => {
+        return <li key={index}>{name}</li>
+      });
+    }
+  }
+  render() {
+    return <ul>{this.renderPosts()}</ul>
+  }
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.domain = 'https://chitter-backend-api.herokuapp.com';
     this.port = '443';
-    this.state = {posts: 'nothing has been fetched yet'};
+    this.state = {posts: []};
   }
 
   componentDidMount() {
@@ -17,12 +31,12 @@ class App extends React.Component {
         return people.map(person => person.handle);
       })
       .then(peopleNames => {
-        this.setState({posts: JSON.stringify(peopleNames)});
+        this.setState({posts: peopleNames});
       });
   }
 
   render() {
-    return <h1>{this.state.posts}</h1>
+    return <Posts names={this.state.posts} />
   }
 }
 
